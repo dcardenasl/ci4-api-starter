1e858e8a925509bc5b139dc898a34e2bf5859d72
David Cardenas Lorca <cine.dcl@gmail.com>
2026-05-12 15:49:12 -0400

    feat(helpers): expose procedural auth helpers wrapping Hasher from ci4-api-core

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -88,5 +88,5 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = ['validation', 'date', 'security', 'request'];
+    public $helpers = ['validation', 'date', 'security', 'request', 'auth'];
 }

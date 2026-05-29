7c6cda8992ee1129f4c50a04a0bdefa4ba1f2e12
David Cardenas Lorca <cine.dcl@gmail.com>
2026-05-29 18:09:54 -0400

    fix(exceptions): restore AppExceptionHandler extends BaseExceptionHandler

--- app/Libraries/Exceptions/AppExceptionHandler.php
+++ app/Libraries/Exceptions/AppExceptionHandler.php
@@ -4,29 +4,8 @@ declare(strict_types=1);
 
 namespace App\Libraries\Exceptions;
 
-use CodeIgniter\Debug\ExceptionHandlerInterface;
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use Throwable;
+use dcardenasl\Ci4ApiCore\Exceptions\BaseExceptionHandler;
 
-class AppExceptionHandler implements ExceptionHandlerInterface
+class AppExceptionHandler extends BaseExceptionHandler
 {
-    public function handle(
-        Throwable $exception,
-        RequestInterface $request,
-        ResponseInterface $response,
-        int $statusCode,
-        int $exitCode,
-    ): void {
-        $response
-            ->setStatusCode($statusCode)
-            ->setContentType('application/json')
-            ->setBody((string) json_encode([
-                'success' => false,
-                'message' => $exception->getMessage() ?: 'An unexpected error occurred.',
-            ], JSON_THROW_ON_ERROR))
-            ->send();
-
-        exit($exitCode);
-    }
 }
